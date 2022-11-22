728dc5ee2afed35a3a093f7cd85001b147e38c18
Gerard <kanetpwnz@gmail.com>
2022-11-22 17:20:55 +0100

    done exercises 02 and 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    var frase = "";
 
-    aaaaaaaaaaaaa
+    while (num > 0) {
+        frase += string;
+        num--;
+      }
+    if (num < 0) {
+        return "ERROR"
+    }
+    return frase;
+}
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+function reverseString(string) {
+    var splitString = string.split("");
+    var reverseArray = splitString.reverse(); 
+    var joinArray = reverseArray.join(""); 
+    return joinArray; 
+}
 
 // Do not edit below this line
 module.exports = reverseString;
